63e0de73e39a263ccd3c3cea4c57187dea96bf42
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-09-05 14:47:52 -0400

    Add toggle navigation

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 //toggle navigation on click
-// function toggleNav() {
-//     var element = document.getElementById("nav");
-//     element.classList.toggle("active");
-// }
+function toggleNav() {
+    var hamburgerElement = document.getElementsByClassName("hamburger")[0];
+    var closeElement = document.getElementsByClassName("close")[0];
+    var mobileLinksElement = document.getElementsByClassName("mobile-links")[0];
+    //hide element by removing style
+    hamburgerElement.classList.toggle("isHidden");
+    document.getElementsByClassName("logo")[0].classList.toggle("isHidden");
+    document.getElementsByClassName("navigation")[0].classList.toggle("isHidden");
+    closeElement.classList.toggle("isHidden");
+    mobileLinksElement.classList.toggle("isHidden");
+
+    console.log("toggleNav");
+}
+
+document.getElementsByClassName("hamburger")[0].addEventListener("click", toggleNav);
+document.getElementsByClassName("close")[0].addEventListener("click", toggleNav);
+document.getElementsByClassName("mobile-links")[0].addEventListener("click", toggleNav);

761d37b7c41f95813742fe29fab10b9307bb25ae
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-09-05 16:30:33 -0400

    Fix linters error

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-//toggle navigation on click
 function toggleNav() {
-    var hamburgerElement = document.getElementsByClassName("hamburger")[0];
-    var closeElement = document.getElementsByClassName("close")[0];
-    var mobileLinksElement = document.getElementsByClassName("mobile-links")[0];
-    //hide element by removing style
-    hamburgerElement.classList.toggle("isHidden");
-    document.getElementsByClassName("logo")[0].classList.toggle("isHidden");
-    closeElement.classList.toggle("isHidden");
-    mobileLinksElement.classList.toggle("isHidden");
-
-    console.log("toggleNav");
+  const hamburgerElement = document.getElementsByClassName('hamburger')[0];
+  const closeElement = document.getElementsByClassName('close')[0];
+  const mobileLinksElement = document.getElementsByClassName('mobile-links')[0];
+  hamburgerElement.classList.toggle('isHidden');
+  document.getElementsByClassName('logo')[0].classList.toggle('isHidden');
+  closeElement.classList.toggle('isHidden');
+  mobileLinksElement.classList.toggle('isHidden');
 }
 
-document.getElementsByClassName("hamburger")[0].addEventListener("click", toggleNav);
-document.getElementsByClassName("close")[0].addEventListener("click", toggleNav);
-document.getElementsByClassName("mobile-links")[0].addEventListener("click", toggleNav);
+document.getElementsByClassName('hamburger')[0].addEventListener('click', toggleNav);
+document.getElementsByClassName('close')[0].addEventListener('click', toggleNav);
+document.getElementsByClassName('mobile-links')[0].addEventListener('click', toggleNav);

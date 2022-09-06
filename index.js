30ef26339cd846aa02825c2e564c014d5780d107
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-09-06 14:03:17 -0400

    Show popup for desktop

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function toggleNav() {
 document.getElementsByClassName('hamburger')[0].addEventListener('click', toggleNav);
 document.getElementsByClassName('close')[0].addEventListener('click', toggleNav);
 document.getElementsByClassName('mobile-links')[0].addEventListener('click', toggleNav);
+
+// Loop throught the elements with className projects and add the event listener
+const projects = document.getElementsByClassName('project');
+console.log(projects);
+for (let i = 0; i < projects.length; i += 1) {
+  document.getElementsByClassName('project')[i].addEventListener('click', () => {
+    document.getElementsByClassName('popup')[0].classList.toggle('isHidden');
+    document.querySelectorAll('section').forEach((section) => {
+      section.classList.toggle('blur');
+    });
+  });
+}
+
+document.getElementsByClassName('close-top-desktop')[0].addEventListener('click', () => {
+  document.getElementsByClassName('popup')[0].classList.toggle('isHidden');
+  document.querySelectorAll('section').forEach((section) => {
+    section.classList.toggle('blur');
+  });
+});
